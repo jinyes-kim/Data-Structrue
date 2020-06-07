11bf128c630ae915d8d1bf975adfe8af0545a291
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-06-07 22:19:24 +0900

    Update 0. Heap.c

--- Review/6. Heap/0. Heap.c
+++ Review/6. Heap/0. Heap.c
@@ -13,31 +13,34 @@ typedef struct HeapType {
 
 //ADT
 HeapType* init_heap(int n);
-element create_node(int value);
-void insert_heap();
-element delete_heap();
+element create_node(int item);
+void insert_max_heap(HeapType* h, element item);
+element delete_max_heap(HeapType* h);
+void heap_sort(element list[], int n);
+void print_heap(HeapType* h);
+void print_list(element list[], int n);
 
 
 HeapType* init_heap(int n) {
     HeapType* tmp = (HeapType*)malloc(sizeof(HeapType));
+    tmp->heap = (element*)malloc(sizeof(HeapType) * n);
     tmp->size = 0;
-    tmp->heap = (element*)malloc(sizeof(element) * (n+1));
     return tmp;
 }
 
-element create_node(int value) {
+element create_node(int item) {
     element tmp;
-    tmp.data = value;
+    tmp.data = item;
     return tmp;
 }
 
-void insert_max_heap(HeapType* h, element node) {    
+void insert_max_heap(HeapType* h, element item) {
     int i = ++(h->size);
-    while ( (i != 1) && (node.data > h->heap[i / 2].data)) {
+    while (i != 1 && item.data > h->heap[i / 2].data) {
         h->heap[i] = h->heap[i / 2];
         i /= 2;
     }
-    h->heap[i] = node;
+    h->heap[i] = item;
 }
 
 element delete_max_heap(HeapType* h) {
@@ -46,54 +49,56 @@ element delete_max_heap(HeapType* h) {
 
     int parent = 1;
     int child = 2;
-
     while (child <= h->size) {
         if (child < h->size &&
             h->heap[child].data < h->heap[child + 1].data) {
             child++;
         }
         if (tmp.data > h->heap[child].data) break;
-        h->heap[parent] = h->heap[child];
-        parent = child;
-        child *= 2;
+        else {
+            h->heap[parent] = h->heap[child];
+            parent = child;
+            child *= 2;
+        }
     }
     h->heap[parent] = tmp;
     return res;
 }
 
-void print_heap(HeapType* h) {
-    int end = h->size;
-    for (int i = 1; i <= end; i++) {
-        printf("%d ", h->heap[i].data);
-    }
-}
-
-void heap_sort(element array[], int n) {
+void heap_sort(element list[], int n) {
     HeapType* h = init_heap(n);
-
-
     for (int i = 0; i < n; i++) {
-        insert_max_heap(h, array[i]);
+        insert_max_heap(h, list[i]);
     }
+    //print_heap(h); 힙에 저장된 상태 
     for (int i = 0; i < n; i++) {
-        array[i] = delete_max_heap(h);
+        list[i] = delete_max_heap(h);
     }
     free(h);
 }
 
-void print_list(element array[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", array[i].data);
+void print_heap(HeapType* h) {
+    for (int i = 1; i <= h->size; i++) {
+        printf("%d ", h->heap[i].data);
     }
     printf("\n");
 }
 
+void print_list(element list[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", list[i].data);
+    }
+    printf("\n");
+}
+
+
 int main(void) {
+    element* data;
     int count;
     int n;
-    printf("개수 => ");
+    printf("리스트 개수 => ");
     scanf_s("%d", &count, 2);
-    element* data = (element*)malloc(sizeof(element) * count);
+    data = (element*)malloc(sizeof(element) * count);
     for (int i = 0; i < count; i++) {
         printf("=> ");
         scanf_s("%d", &n, 2);
