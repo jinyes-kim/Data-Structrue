7bf85b2e8a26349509aeebb440cc102b40ba75be
Kim Jinung <54028026+jinyes-kim@users.noreply.github.com>
2020-06-14 20:12:41 +0900

    Update 트리 연습.c

--- Review/5. Tree/트리 연습.c
+++ Review/5. Tree/트리 연습.c
@@ -1,27 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-basic 코드와 다른점은 순회에서 끝 처리 방법
-*/
-
-
 typedef int element;
 
-typedef struct treeNode {
+typedef struct tree {
     element data;
-    struct treeNode* left;
-    struct treeNode* right;
-} treeNode;
-
+    struct tree* left;
+    struct tree* right;
+} tree;
 
 //ADT
-void preorder(treeNode* root);
-void inorder(treeNode* root);
-void postorder(treeNode* root);
+tree* insert_node(tree* root, element item);
+tree* delete_node(tree* root, element item);
+tree* get_min_node(tree* root);
+tree* new_node(element item);
+void preorder(tree* root);
 
+tree* construct_node(int n);
 
-void preorder(treeNode* root) {
+tree* new_node(element item) {
+    tree* tmp = (tree*)malloc(sizeof(tree));
+    tmp->data = item;
+    tmp->right = tmp->left = NULL;
+    return tmp;
+}
+
+tree* insert_node(tree* root, element item) {
+    if (root == NULL) return new_node(item);
+
+    if (root->data < item) {
+        root->right = insert_node(root->right, item);
+    }
+    else if (root->data > item) {
+        root->left = insert_node(root->left, item);
+    }
+    return root;
+}
+
+tree* get_min_node(tree* root) {
+    tree* tmp = root;
+    while (tmp->left != NULL) {
+        tmp = tmp->left;
+    }
+    return tmp;
+}
+
+tree* delete_node(tree* root, element item) {
+    if (root == NULL) return;
+
+    if (root->data < item) {
+        root->right = delete_node(root->right, item);
+    }
+    else if (root->data > item) {
+        root->left = delete_node(root->left, item);
+    }
+    else {
+        if (root->left == NULL) {
+            tree* tmp = root->right;
+            free(root);
+            return tmp;
+        }
+        else if (root->right == NULL) {
+            tree* tmp = root->left;
+            free(root);
+            return tmp;
+        }
+        else {
+            tree* tmp = get_min_node(root->right);
+            root->data = tmp->data;
+            root->right = delete_node(root->right, tmp->data);
+        }
+    }
+    return root;
+}
+
+void preorder(tree* root) {
     if (root != NULL) {
         printf("%d ", root->data);
         preorder(root->left);
@@ -29,36 +82,30 @@ void preorder(treeNode* root) {
     }
 }
 
-void inorder(treeNode* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        printf("%d ", root->data);
-        inorder(root->right);
+tree* construct_node(int n) {
+    int count = 0;
+    int input;
+    tree* tmp = NULL;
+    for (; count < n; count++) {
+        printf("삽입 노드: ");
+        scanf_s("%d", &input);
+        tmp = insert_node(tmp, input);
     }
+    return tmp;
 }
 
-void postorder(treeNode* root) {
-    if (root != NULL) {
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d ", root->data);
-    }
-}
 
 int main(void) {
-    treeNode n7 = {7, NULL, NULL };
-    treeNode n6 = {6, NULL, NULL};
-    treeNode n5 = {5, NULL, NULL};
-    treeNode n4 = {4, NULL, NULL};
-    treeNode n3 = {3, &n6, &n7};
-    treeNode n2 = {2, &n4, &n5};
-    treeNode n1 = {1, &n2, &n3};
+    tree* root = construct_node(6);
 
-    postorder(&n1);
+    preorder(root);
     printf("\n");
-    inorder(&n1);
-    printf("\n");
-    preorder(&n1);
-    printf("\n");
+
+    root = delete_node(root, 4);
+    root = delete_node(root, 5);
+    root = delete_node(root, 1);
+
+    preorder(root);
+
     return 0;
 }
